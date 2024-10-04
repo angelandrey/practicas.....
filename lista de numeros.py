8f5dc23061c505240f0b5efd74816f0690f90b27
Angel Andrey Muñoz Centeno <a.23308051281202@cbtis128.edu.mx>
2024-10-04 16:55:15 -0600

    Add files via upload

---
+++ lista de numeros.py
@@ -0,0 +1,18 @@
+print("Angel Andrey Muñoz Centeno :   3W")  # INFO DE PRACTICA Y PROGRAMADOR
+print(" ")  # Espacio
+
+# Lista de numeros
+numeros = [43, 57, 92, 20, 37, 5, 9]
+
+# Ordenar la lista de mayor a menor
+numeros.sort(reverse=True)
+
+# Mostrar la lista ordenada de arriba hacia abajo
+print("Lista de precios de mayor a menor:")
+for numeros in numeros:
+    print(numeros)
+
+print(" ")
+# Mostrar el mayor y el menor numero
+print("EL MAYOR NUMERO ES : 92")
+print("EL MENOR NUMERO ES : 5")
